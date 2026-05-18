96d721bbff6d5225cc0fe663e4cdeea92900831a
pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-05-18 13:41:49 +0900

    chapter9

---
+++ src/chap9.lisp
@@ -0,0 +1,42 @@
+(defvar *test-name* nil)
+
+(defun report-result (result form)
+  (format t "~:[FAIL~;pass~] ... ~a: ~a~%" result *test-name* form)
+  result)
+
+(defmacro check (&body forms)
+  `(combine-results
+     ,@(loop for f in forms collect `(report-result ,f ',f))))
+
+(defmacro with-gensyms ((&rest names) &body body)
+  `(let
+       ,(loop for n in names
+              collect `(,n (gensym)))
+     ,@body))
+
+(defmacro combine-results (&body forms)
+  (with-gensyms (result)
+    `(let ((,result t))
+       ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
+       ,result)))
+
+(defmacro deftest (name parameters &body body)
+  `(defun ,name ,parameters
+     (let ((*test-name* ',name))
+       ,@body)))
+
+(deftest test-+ ()
+  (check
+    (= (+ 1 2) 3)
+    (= (+ 1 2 3) 6)
+    (= (+ -1 -3) -4)))
+
+(deftest test-* ()
+  (check
+    (= (* 2 2) 4)
+    (= (* 3 5) 15)))
+
+(defun test-arithmetic ()
+  (combine-results
+    (test-+)
+    (test-*)))
